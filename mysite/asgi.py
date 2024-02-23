f9ebee36e2fe86ca126d788f6721d670889a5de3
tommyBurgos <trburgos@espol.edu.ec>
2024-02-22 19:08:52 -0500

    se quita favicon

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -14,3 +14,16 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_asgi_application()
+"""
+import os
+import sys
+
+path=os.path.expanduser('~/Proyecto-Pulseras')
+if path not in sys.path:
+    sys.path.insert(0,path)
+    os.environ['DJANGO_SETTINGS_MODULE']='mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+aplication= StaticFilesHandler(get_wsgi_application())
+"""
