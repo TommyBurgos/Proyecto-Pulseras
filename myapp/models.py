966bf295db7874aef18340c6bcf6d2c98020b43e
tommyBurgos <trburgos@espol.edu.ec>
2024-04-14 22:39:17 -0500

    Finalmente resolucion de errores y se agregan archivos gitignore y requeriments para evitar futuros daños y conflictos

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+class Enfermedades(models.Model):
+    nombre= models.CharField(max_length=100)
+    descripcion= models.CharField(max_length=150)
+
+class tipoAlerta(models.Model):
+    nombre= models.CharField(max_length=100)
+    descripcion= models.CharField(max_length=200)
+
+class Alerta(models.Model):
+    nombre= models.CharField(max_length=100)
+    descripcion= models.CharField(max_length=200)
+    tipoAlerta=models.ForeignKey(tipoAlerta,on_delete=models.CASCADE)
 
 class Medicamento(models.Model):
     name= models.CharField(max_length=100)
@@ -15,6 +27,15 @@ class Departamento(models.Model):
 class Medico(models.Model):
     idUsuario=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     departamento=models.ForeignKey(Departamento,on_delete=models.CASCADE)
+
+class Especialidad(models.Model):
+    nombre=models.CharField(max_length=100)
+    descripcion=models.CharField(max_length=200)
+
+class MedicoEspecialidad(models.Model):
+    idMedico=models.ForeignKey(Medico,on_delete=models.CASCADE)
+    idEspecialidad=models.ForeignKey(Especialidad,on_delete=models.CASCADE)
+    fecha=models.DateField()
 
 class EstadoCita(models.Model):
     nombre=models.CharField(max_length=100)
@@ -47,6 +68,41 @@ class Paciente(models.Model):
     idDispositivo=models.ForeignKey(Dispositivo,on_delete=models.CASCADE)
     estado=models.ForeignKey(Estado,on_delete=models.CASCADE)
 
+class pacienteDispositivo(models.Model):
+    idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
+    idDispositivo=models.ForeignKey(Dispositivo,on_delete=models.CASCADE,blank=True, null=True)
+    fechaRegistro=models.DateField()
+    fechaModificación=models.DateField()
+
+class CercoVirtual(models.Model):
+    latitud = models.DecimalField(max_digits=9, decimal_places=6)
+    longitud = models.DecimalField(max_digits=9, decimal_places=6)
+
+class CercoPaciente(models.Model):
+    idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
+    idCerco=models.ForeignKey(CercoVirtual,on_delete=models.CASCADE,blank=True, null=True)
+    fecha=models.DateField()
+    
+class RolFamiliar(models.Model):    
+    nombre=models.CharField(max_length=100)
+    descripcion=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
+    
+class AntecedentesQuirurgicos(models.Model):
+    idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
+    fecha=models.DateField()
+    descripcion=models.CharField(max_length=200)
+
+class Familiar(models.Model):    
+    idUsuario=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True, null=True)
+    rolFamiliar=models.ForeignKey(RolFamiliar,on_delete=models.CASCADE,blank=True, null=True)    
+
+class FamiliarPaciente(models.Model):
+    idFamiliar=models.ForeignKey(Familiar,on_delete=models.CASCADE,blank=True, null=True)
+    idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
+
 
 class RecetaMedica(models.Model):
     fechaEmision=models.DateField()
@@ -64,6 +120,27 @@ class CitaMedica(models.Model):
     fechaCita=models.DateTimeField(auto_now_add=True)
     motivo=models.CharField(max_length=250)
 
+class BlogNoticia(models.Model):
+    titulo=models.CharField(max_length=200)
+    noticia=models.CharField(max_length=1000)
+class CentroMedico(models.Model):
+    nombre= models.CharField(max_length=100)
+    direccion= models.CharField(max_length=150)
+    medicos = models.ManyToManyField(Medico)#Para que la relacion sea muchos a uno
+    blog = models.ManyToManyField(BlogNoticia)
+#Puedo probar
+# Crear un centro médico
+#centro_medico = CentroMedico.objects.create(nombre="Hospital XYZ", direccion="123 Calle Principal")
+# Crear algunos médicos
+#medico1 = Medico.objects.create(nombre="Dr. Juan Pérez")
+#medico2 = Medico.objects.create(nombre="Dra. María Gómez")
+# Asociar los médicos al centro médico
+#centro_medico.medicos.add(medico1, medico2)
+
+
+# Create your models here.
+
+
 # Create your models here.
 class Paquete(models.Model):
     name= models.CharField(max_length=200)
@@ -72,18 +149,33 @@ class Paquete(models.Model):
 
     def __str__(self):
         return self.name
+class DetalleServicio(models.Model):
+    fecha=models.DateField()
+    hora = models.TimeField()
+    medicion=models.CharField(max_length=200)
+
 class Servicio(models.Model):
     nombre= models.CharField(max_length=200)
-    descripcion=models.TextField(blank=True)
-    paquete=models.ForeignKey(Paquete, on_delete=models.CASCADE)
+    umbral=models.TextField(blank=True)
+    detalleServicio=models.ForeignKey(DetalleServicio, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.nombre + '-'+ self.paquete.name
+        return self.nombre + '-'+ self.detalleServicio.medicion
+
 
 class detallePaquete(models.Model):
     idPaquete=models.ForeignKey(Paquete,on_delete=models.CASCADE,blank=True)
     idServicio=models.ForeignKey(Servicio,on_delete=models.CASCADE,blank=True)
-    
+
+class PacientePaquete(models.Model):
+    idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True)
+    idPaquete=models.ForeignKey(Paquete,on_delete=models.CASCADE,blank=True)
+    idDispositivo=models.ForeignKey(Dispositivo,on_delete=models.CASCADE,blank=True)
+    fechaInicio=models.DateField()
+    fechaFin=models.DateField()
+    precio=models.FloatField()
+    estado=models.ForeignKey(Estado,on_delete=models.CASCADE,blank=True)
+
     
 
 
