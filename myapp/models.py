c8173c781e356228059c19d077c2cb7e63de36a5
ggpachec <ggpachec@espol.edu.ec>
2024-06-10 22:16:02 -0500

    blog y cambios en models.py

--- myapp/models.py
+++ myapp/models.py
@@ -6,15 +6,6 @@ class Enfermedades(models.Model):
     nombre= models.CharField(max_length=100)
     descripcion= models.CharField(max_length=150)
 
-class tipoAlerta(models.Model):
-    nombre= models.CharField(max_length=100)
-    descripcion= models.CharField(max_length=200)
-
-class Alerta(models.Model):
-    nombre= models.CharField(max_length=100)
-    descripcion= models.CharField(max_length=200)
-    tipoAlerta=models.ForeignKey(tipoAlerta,on_delete=models.CASCADE)
-
 class Medicamento(models.Model):
     name= models.CharField(max_length=100)
     indicaciones= models.CharField(max_length=150)
@@ -50,19 +41,23 @@ class TipoDispositivo(models.Model):
 
     def __str__(self):
         return self.nombre
-    
+
 class EstadoDispositivo(models.Model):
     nombre=models.CharField(max_length=100)
     descripcion=models.CharField(max_length=200)
 
     def __str__(self):
         return self.nombre
-    
+
 class Dispositivo(models.Model):
     tipo=models.ForeignKey(TipoDispositivo,on_delete=models.CASCADE)
     estadoD=models.ForeignKey(EstadoDispositivo,on_delete=models.CASCADE)
     serie=models.CharField(max_length=50)
-    
+    modelo=models.CharField(max_length=200, default="Modelo por default")
+    color=models.CharField(max_length=150, default="Negro")
+    version=models.CharField(max_length=250, default="Version 1.0.0")
+    imgD=models.ImageField(upload_to='dispositivo_images/', default='dispositivo1.jpg')
+
 class Paciente(models.Model):
     idUsuario=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     idDispositivo=models.ForeignKey(Dispositivo,on_delete=models.CASCADE)
@@ -72,7 +67,7 @@ class pacienteDispositivo(models.Model):
     idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
     idDispositivo=models.ForeignKey(Dispositivo,on_delete=models.CASCADE,blank=True, null=True)
     fechaRegistro=models.DateField()
-    fechaModificación=models.DateField()
+    fechaModificacion=models.DateField()
 
 class CercoVirtual(models.Model):
     latitud = models.DecimalField(max_digits=9, decimal_places=6)
@@ -82,22 +77,22 @@ class CercoPaciente(models.Model):
     idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
     idCerco=models.ForeignKey(CercoVirtual,on_delete=models.CASCADE,blank=True, null=True)
     fecha=models.DateField()
-    
-class RolFamiliar(models.Model):    
+
+class RolFamiliar(models.Model):
     nombre=models.CharField(max_length=100)
     descripcion=models.CharField(max_length=200)
 
     def __str__(self):
         return self.nombre
-    
+
 class AntecedentesQuirurgicos(models.Model):
     idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
     fecha=models.DateField()
     descripcion=models.CharField(max_length=200)
 
-class Familiar(models.Model):    
+class Familiar(models.Model):
     idUsuario=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True, null=True)
-    rolFamiliar=models.ForeignKey(RolFamiliar,on_delete=models.CASCADE,blank=True, null=True)    
+    rolFamiliar=models.ForeignKey(RolFamiliar,on_delete=models.CASCADE,blank=True, null=True)
 
 class FamiliarPaciente(models.Model):
     idFamiliar=models.ForeignKey(Familiar,on_delete=models.CASCADE,blank=True, null=True)
@@ -108,6 +103,7 @@ class RecetaMedica(models.Model):
     fechaEmision=models.DateField()
     idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE)
     idMedico=models.ForeignKey(Medico,on_delete=models.CASCADE)
+    receta=models.CharField(max_length=1000, default="Consulte con su médico")
 
 class DetalleReceta(models.Model):
     idReceta=models.ForeignKey(RecetaMedica,on_delete=models.CASCADE)
@@ -118,12 +114,15 @@ class CitaMedica(models.Model):
     idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE)
     idMedico=models.ForeignKey(Medico,on_delete=models.CASCADE)
     fechaCita=models.DateTimeField(auto_now_add=True)
+    horaCita=models.TimeField(blank=True)
     motivo=models.CharField(max_length=250)
 
 class BlogNoticia(models.Model):
+    fecha=models.DateTimeField(auto_now_add=True)
+    img=models.ImageField(upload_to='blog_images/', default='blog1.jpg')
     titulo=models.CharField(max_length=200)
-    noticia=models.CharField(max_length=1000)
-    
+    noticia=models.CharField(max_length=100000)
+
 class CentroMedico(models.Model):
     nombre= models.CharField(max_length=100)
     direccion= models.CharField(max_length=150)
@@ -150,6 +149,7 @@ class Paquete(models.Model):
 
     def __str__(self):
         return self.name
+
 class DetalleServicio(models.Model):
     fecha=models.DateField()
     hora = models.TimeField()
@@ -177,7 +177,20 @@ class PacientePaquete(models.Model):
     precio=models.FloatField()
     estado=models.ForeignKey(Estado,on_delete=models.CASCADE,blank=True)
 
-    
+class tipoAlerta(models.Model):
+    nombre= models.CharField(max_length=100)
+    descripcion= models.CharField(max_length=200)
+
+class Alerta(models.Model):
+    idPaciente=models.ForeignKey(Paciente,on_delete=models.CASCADE,blank=True, null=True)
+    nombre= models.CharField(max_length=100)
+    mensaje= models.TextField(default="Default")
+    tipoAlerta=models.ForeignKey(tipoAlerta,on_delete=models.CASCADE)
+    fechaCreacion= models.DateTimeField(auto_now_add=True)
+    activa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.mensaje
 
 
 
